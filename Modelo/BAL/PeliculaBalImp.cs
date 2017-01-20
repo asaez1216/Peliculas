5e424f1da3e7b7ce07fd0e7351e28e92e286e70e
asaez1216 <asaez1216@gmail.com>
2017-01-20 17:44:30 -0300

    Secreo la capa de acceso

---
+++ Modelo/BAL/PeliculaBalImp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo.DAL;
+using Modelo.ET;
+
+namespace Modelo.BAL
+{
+    class PeliculaBalImp:IPeliculaBal
+    {
+        private IPeliculaDal peliculaDalImp = new PeliculaDalImp();
+
+        public List<Pelicula> Listar()
+        {
+            return peliculaDalImp.Listar();
+        }
+    }
+}
---
+++ Modelo/DAL/PeliculaDalImp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo.ET;
+using System.Configuration;
+
+namespace Modelo.DAL
+{
+    class PeliculaDalImp:IPeliculaDal
+    {
+        public List<Pelicula> Listar()
+        {
+            var peliculas = new List<Pelicula>();
+            
+            try
+            {
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["Presupuesto_corriente"].ToString()))
+                {
+                    con.Open();
+
+                    var query = new SqlCommand("SELECT * FROM peliculas", con);
+                    using (var dr = query.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            // Usuario
+                            var pelicula = new Pelicula
+                            {
+                                PeliculaId = Convert.ToInt32(dr["peliculaId"]),
+                                NombrePelicula = dr["nombrePelicula"].ToString(),
+                                NombreDirector = dr["nombreDirector"].ToString(),
+                                AcnoEstreno = Convert.ToInt32(dr["saldo"])
+
+                            };
+
+                            // Agregamos el usuario a la lista genreica
+                            peliculas.Add(pelicula);
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return peliculas;
+        }
+
+
+    }
+    }
+}
