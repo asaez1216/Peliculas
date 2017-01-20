cd82ab36c0fd9e7ff7326b1d5d8e85328f3d2f71
asaez1216 <asaez1216@gmail.com>
2017-01-20 17:02:32 -0300

    Se creo la estructura del proyecto. Se agregó una libreria de clases separada

---
+++ Modelo/ET/Pelicula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.ET
+{
+    class Pelicula
+    {
+        public int PeliculaId { get; set; }
+        public string NombrePelicula { get; set; }
+        public string NombreDirector { get; set; }
+        public int AcnoEstreno { get; set; } 
+    }
+}
